19bf6dd97941dbfba7c9a7dc596922c2b59c275c
itu-itis23-cevikf22 <ITU_cevikf22@itu.edu.tr>
2024-12-27 21:43:53 +0300

    A

--- app2.py
+++ app2.py
@@ -3,61 +3,61 @@ from transformers import pipeline
 import os
 
 
-# FFmpeg'in bulunduğu dizini PATH'e ekle
+# Hi Teacher, I had to add this "ffmpeg" path, I could not find another way.
 os.environ["PATH"] += os.pathsep + r"C:\Users\Faruk\Downloads\ffmpeg-master-latest-win64-gpl-shared 2\ffmpeg-master-latest-win64-gpl-shared\bin"
 
 # ------------------------------
-# Whisper Modelini Yükle
+# Whisper Model
 # ------------------------------
 def load_whisper_model():
     """
-    Whisper modelini yükler ve geri döner.
+    Loads the Whisper Model.
     """
     model = pipeline("automatic-speech-recognition", model="openai/whisper-tiny")
     return model
 
 # ------------------------------
-# NER Modelini Yükle 
+# NER Model 
 # ------------------------------
 def load_ner_model():
     """
-    NER modelini yükler ve geri döner.
+    Loads the NER Model.
     """
     ner_model = pipeline("ner", model="dslim/bert-base-NER", aggregation_strategy="simple")
     return ner_model
 
 # ------------------------------
-# Ses Dosyasını Metne Dönüştür
+# Transcribe audio to text
 # ------------------------------
 def transcribe_audio(uploaded_file, model):
     """
-    Yüklenen ses dosyasını Whisper modelini kullanarak metne dönüştürür.
+    Transcribes the audio file to a text using Whisper Model.
     """
     audio = uploaded_file.read()
     transcription = model(audio, return_timestamps=True)
     return transcription["text"]
 
 # ------------------------------
-# Varlık Çıkartma
+# Extract entities
 # ------------------------------
 def extract_entities(text, ner_model):
     """
-    Metinden kişileri (PERs), organizasyonları (ORGs) ve lokasyonları (LOCs) çıkarır.
+    Extracts persons (PERs), organizations (ORGs) and locations (LOCs) from the text.
     """
     entities = ner_model(text)
     
-    # Organizasyonları (ORGs) çıkar
+    # Extract Organizations (ORGs)
     orgs = set([entity['word'] for entity in entities if entity['entity_group'] == 'ORG'])
     
-    # Lokasyonları (LOCs) çıkar
+    # Extract Locations (LOCs) 
     locs = set([entity['word'] for entity in entities if entity['entity_group'] == 'LOC'])
     
-    # Kişileri (PERs) çıkar
+    # Extract persons (PERs) 
     pers = set([entity['word'] for entity in entities if entity['entity_group'] == 'PER'])
     
     return orgs, locs, pers
 # ------------------------------
-# Streamlit Uygulaması
+# Streamlit Application
 # ------------------------------
 def main():
     st.title("Extracting Text and Entities From Audio")
@@ -67,28 +67,27 @@ def main():
     STUDENT_ID = "150220325"
     st.write(f"**Student ID: {STUDENT_ID} - {STUDENT_NAME}**")
 
-    # Dosya Yükleyici
+   
     uploaded_file = st.file_uploader("Upload the audio file", type=["wav"])
     
     if uploaded_file is not None:
-        st.audio(uploaded_file, format="audio/wav")  # Ses dosyası
+        st.audio(uploaded_file, format="audio/wav")  # Audio File
         
-        # Whisper modeli
+        # Whisper model
         whisper_model = load_whisper_model()
         
-        # NER modeli
+        # NER model
         ner_model = load_ner_model()
         
-        # Ses kaydını metne dönüştür
+        
         transcribed_text = transcribe_audio(uploaded_file, whisper_model)
         st.write("# Transcription")
         st.text(transcribed_text)
 
-        # Varlıkları çıkar
         st.write("# Extracted Entities:")
         orgs, locs, pers = extract_entities(transcribed_text, ner_model)
         
-        # Varlıkları gruplandır ve göster
+        # Group and display the entities
         st.write("## Organizations (ORGs):")
         for entity in orgs:
             st.write(f"• {entity}")
